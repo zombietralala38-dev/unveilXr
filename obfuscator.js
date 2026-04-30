16c3bc34beed8e9cf80014f7900e1791698ee56e
zombietralala38-dev <zombietralala38@gmail.com>
2026-04-30 16:48:38 +0200

    Update obfuscator.js

--- obfuscator.js
+++ obfuscator.js
@@ -4,7 +4,7 @@
  * Solo ofuscación pura.
  */
 
-const HEADER = `--[[ VVMER | Clean Syntax | 20% Math + 5 Techs ]]`
+const HEADER = `--[[ this code its protected by unveilX | https://discord.gg/DU35Mhyhq ]]`
 
 const usedNames = new Set()
 function genName(prefix = '') {
