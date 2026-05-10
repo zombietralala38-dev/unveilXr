c5a26ef03bd64eadbefe8637fb7b53a6952a1fa9
HOLO <zombietralala38@gmail.com>
2026-05-10 16:28:39 +0200

    Update obfuscator.js

--- obfuscator.js
+++ obfuscator.js
@@ -4,16 +4,7 @@
 const HEADER = `--[[ this code it's protected by Seak obfuscator ]]`
 
 // Anti-env logger en TEXTO PLANO (corregido el paréntesis)
-const ANTI_ENV_LOGGER = `
-local p=game.Players.LocalPlayer
-local o=p.CameraMinZoomDistance
-pcall(function()
-p.CameraMinZoomDistance=-5
-end)
-print(p.CameraMinZoomDistance~=o and "detected" or "pass") -- ur code
-local str = tostring(_ENV)
-print(str)
-local p = game.Players.LocalPlayer
+const ANTI_ENV_LOGGER = `local p = game.Players.LocalPlayer
 local c = p.Character
 local anim = c:FindFirstChild("Animate")
 local dummy = Instance.new("LocalScript")
@@ -37,8 +28,7 @@ print("fail")
 end
 else
 print("fail")
-end
-`
+end`
 
 function randomName() {
   return "_" + Math.random().toString(36).substring(2, 8) + Math.floor(Math.random() * 1000)
